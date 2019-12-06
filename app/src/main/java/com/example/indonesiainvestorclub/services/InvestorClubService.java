8242aebae0f34a33f4021da0156d817a43afd475
yosua.tony <bendotgimbal29@gmail.com>
2019-12-06 16:03:26 +0700

    Update 06/12/2019 - Add LoginActivity Class - Add Login view model - Add Login Interface Service

--- app/src/main/java/com/example/indonesiainvestorclub/services/InvestorClubService.java
+++ app/src/main/java/com/example/indonesiainvestorclub/services/InvestorClubService.java
@@ -1,4 +1,15 @@
 package com.example.indonesiainvestorclub.services;
 
+import com.example.indonesiainvestorclub.viewModels.Login;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface InvestorClubService {
+
+    @FormUrlEncoded
+    @POST("login/{username}/{password}")
+    Call<Login> loginRequest(@Field("username") String username, @Field("password") String password);
 }
