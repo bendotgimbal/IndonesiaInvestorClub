5304ffa4ca6418b6da7e4c1570a31cbf54d52f37
yosua.tony <bendotgimbal29@gmail.com>
2019-12-08 00:48:59 +0700

    Update 08/12/2019 - Add LoginActivity Class

--- app/src/main/java/com/example/indonesiainvestorclub/views/LoginActivity.java
+++ app/src/main/java/com/example/indonesiainvestorclub/views/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.indonesiainvestorclub.R;
+import com.example.indonesiainvestorclub.services.InvestorClubService;
+import com.example.indonesiainvestorclub.services.ServiceGenerator;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +14,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        InvestorClubService service = ServiceGenerator.service;
+        InvestorClubService mInterfaceService = ServiceGenerator.retrofit.create(InvestorClubService.class);
     }
 }
