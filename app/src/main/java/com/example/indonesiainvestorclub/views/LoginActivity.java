582b83370911345829e73b81a1995d8dfdf01aae
robertw <robert.w.ruhulessin@alfacart.com>
2019-12-08 01:04:49 +0700

    Initial project setting + Login activity recode

--- app/src/main/java/com/example/indonesiainvestorclub/views/LoginActivity.java
+++ app/src/main/java/com/example/indonesiainvestorclub/views/LoginActivity.java
@@ -2,20 +2,27 @@ package com.example.indonesiainvestorclub.views;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
 import com.example.indonesiainvestorclub.R;
-import com.example.indonesiainvestorclub.services.InvestorClubService;
-import com.example.indonesiainvestorclub.services.ServiceGenerator;
+import com.example.indonesiainvestorclub.databinding.LoginActivityBinding;
+import com.example.indonesiainvestorclub.viewModels.LoginViewModel;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+    private LoginActivityBinding binding;
+    private LoginViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+    }
 
-        InvestorClubService service = ServiceGenerator.service;
-        InvestorClubService mInterfaceService = ServiceGenerator.retrofit.create(InvestorClubService.class);
+    @Override
+    public void initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.login_activity);
+        viewModel = new LoginViewModel(this, binding);
+        binding.setViewModel(viewModel);
+
     }
 }
+
