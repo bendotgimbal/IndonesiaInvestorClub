031e2942e8c854c6acfd0dca0746b86330ede4c8
robertw <robert.w.ruhulessin@alfacart.com>
2019-12-10 17:25:16 +0700

    ENHANCE LAYOUT & ADD API CALL + drawer on main activity + added api call on profile

--- app/src/main/java/com/example/indonesiainvestorclub/viewModels/MainViewModel.java
+++ app/src/main/java/com/example/indonesiainvestorclub/viewModels/MainViewModel.java
@@ -1,10 +1,61 @@
 package com.example.indonesiainvestorclub.viewModels;
 
 import android.content.Context;
+import android.content.Intent;
+import androidx.core.view.GravityCompat;
+import com.example.indonesiainvestorclub.R;
+import com.example.indonesiainvestorclub.databinding.ActivityMainBinding;
+import com.example.indonesiainvestorclub.views.AgreementActivity;
+import com.example.indonesiainvestorclub.views.LoginActivity;
+import com.example.indonesiainvestorclub.views.PerformanceActivity;
+import com.example.indonesiainvestorclub.views.ProfileActivity;
 
 public class MainViewModel extends BaseViewModel {
 
-  public MainViewModel(Context context) {
+  private ActivityMainBinding binding;
+
+  public MainViewModel(Context context, ActivityMainBinding binding) {
     super(context);
+    this.binding = binding;
+    initNavMenu();
+  }
+
+  private void initNavMenu() {
+    binding.navView.setNavigationItemSelectedListener(menuItem -> {
+      int id = menuItem.getItemId();
+      switch (id) {
+        case R.id.nav_home:
+          goToMenu(PerformanceActivity.class);
+          break;
+        case R.id.nav_login:
+          goToMenu(LoginActivity.class);
+          break;
+        case R.id.nav_profile:
+          goToMenu(ProfileActivity.class);
+          break;
+        case R.id.nav_portofolio:
+          goToMenu(AgreementActivity.class);
+          break;
+      }
+      return true;
+    });
+  }
+
+  private void goToMenu(Class activity){
+    closeDrawer();
+    context.startActivity(new Intent(context, activity));
+  }
+
+  @SuppressWarnings("unused")
+  public void openDrawer() {
+    binding.drawerLayout.isDrawerOpen(GravityCompat.START);
+  }
+
+  public void closeDrawer() {
+    binding.drawerLayout.closeDrawer(GravityCompat.START, true);
+  }
+
+  public boolean isDrawerOpen(){
+    return binding.drawerLayout.isDrawerOpen(GravityCompat.START);
   }
 }
--- app/src/main/java/com/example/indonesiainvestorclub/views/MainActivity.java
+++ app/src/main/java/com/example/indonesiainvestorclub/views/MainActivity.java
@@ -1,72 +1,50 @@
 package com.example.indonesiainvestorclub.views;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.databinding.DataBindingUtil;
 import com.example.indonesiainvestorclub.R;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import com.example.indonesiainvestorclub.databinding.ActivityMainBinding;
 import com.example.indonesiainvestorclub.viewModels.MainViewModel;
-import com.google.android.material.navigation.NavigationView;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends BaseActivity {
 
-  private AppBarConfiguration mAppBarConfiguration;
-  //private MainActivityBinding binding;
+  private ActivityMainBinding binding;
   private MainViewModel viewModel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    setSupportActionBar(binding.appbar.toolbar);
+    initDrawerIcon();
+  }
 
-    DrawerLayout drawer = findViewById(R.id.drawer_layout);
-    NavigationView navigationView = findViewById(R.id.nav_view);
-    mAppBarConfiguration = new AppBarConfiguration.Builder(
-        R.id.nav_home, R.id.nav_login, R.id.nav_profile, R.id.nav_portofolio
-    )
-        .setDrawerLayout(drawer)
-        .build();
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-    NavigationUI.setupWithNavController(navigationView, navController);
-
-    navigationView.setNavigationItemSelectedListener(menuItem -> {
-      int id = menuItem.getItemId();
-      switch (id) {
-        case R.id.nav_home:
-          Toast.makeText(MainActivity.this, "MainActivity", Toast.LENGTH_SHORT).show();
-          startActivity(new Intent(MainActivity.this, PerformanceActivity.class));
-        case R.id.nav_login:
-          Toast.makeText(MainActivity.this, "LoginActivity", Toast.LENGTH_SHORT).show();
-          startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        case R.id.nav_profile:
-          Toast.makeText(MainActivity.this, "ProfileActivity", Toast.LENGTH_SHORT).show();
-          startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-        case R.id.nav_portofolio:
-          Toast.makeText(MainActivity.this, "AgreementActivity", Toast.LENGTH_SHORT).show();
-          startActivity(new Intent(MainActivity.this, AgreementActivity.class));
-      }
-      return true;
-    });
+  private void initDrawerIcon() {
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      getSupportActionBar().setHomeButtonEnabled(true);
+      ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+          binding.drawerLayout,
+          binding.appbar.toolbar,
+          R.string.navigation_drawer_open,
+          R.string.navigation_drawer_close);
+      binding.drawerLayout.addDrawerListener(toggle);
+      toggle.syncState();
+    }
   }
 
   @Override
   public void initDataBinding() {
-
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    viewModel = new MainViewModel(this, binding);
+    binding.setViewModel(viewModel);
   }
 
-  @Override
-  public boolean onSupportNavigateUp() {
-    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-        || super.onSupportNavigateUp();
+  @Override public void onBackPressed() {
+    if (viewModel.isDrawerOpen()) {
+      viewModel.closeDrawer();
+      return;
+    }
+    super.onBackPressed();
   }
 }
