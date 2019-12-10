eb9f6db985cc9c5a06b1de33b601383cb4675da4
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2019-12-10 10:00:00 +0700

    Update 10/12/2019 - Class Performance Request

--- app/src/main/java/com/example/indonesiainvestorclub/services/InvestorClubService.java
+++ app/src/main/java/com/example/indonesiainvestorclub/services/InvestorClubService.java
@@ -1,9 +1,12 @@
 package com.example.indonesiainvestorclub.services;
 
 import com.example.indonesiainvestorclub.models.response.LoginRes;
+import com.example.indonesiainvestorclub.models.response.PerformanceRes;
+
 import io.reactivex.Observable;
 import okhttp3.RequestBody;
 import retrofit2.Response;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -13,4 +16,7 @@ public interface InvestorClubService {
   @Multipart
   @POST("login")
   Observable<Response<LoginRes>> loginRequest(@Part("username") RequestBody username, @Part("password") RequestBody password);
+
+  @GET("public/performance")
+  Observable<Response<PerformanceRes>> performanceRequest();
 }
--- app/src/main/java/com/example/indonesiainvestorclub/views/PerformanceActivity.java
+++ app/src/main/java/com/example/indonesiainvestorclub/views/PerformanceActivity.java
@@ -1,4 +1,28 @@
 package com.example.indonesiainvestorclub.views;
 
-public class PerformanceActivity {
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+
+import com.example.indonesiainvestorclub.R;
+import com.example.indonesiainvestorclub.databinding.PerformanceActivityBinding;
+import com.example.indonesiainvestorclub.viewModels.PerformanceViewModel;
+
+public class PerformanceActivity extends BaseActivity {
+
+    private PerformanceActivityBinding binding;
+    private PerformanceViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.performance_activity);
+        viewModel = new PerformanceViewModel(this, binding);
+        binding.setViewModel(viewModel);
+
+    }
 }
