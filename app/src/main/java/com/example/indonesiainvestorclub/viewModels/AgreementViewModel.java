d70f1f0a20a07a3b3bcdcb5685cc2387d227c043
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2019-12-19 16:18:47 +0700

    Update 19/12/2019 10:41
    - Update AgreementViewModel
    - Add Icon InvestorClub

--- app/src/main/java/com/example/indonesiainvestorclub/viewModels/AgreementViewModel.java
+++ app/src/main/java/com/example/indonesiainvestorclub/viewModels/AgreementViewModel.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.example.indonesiainvestorclub.databinding.AgreementFragmentBinding;
 import com.example.indonesiainvestorclub.helper.SharedPreferenceHelper;
+import com.example.indonesiainvestorclub.models.Agreement;
 import com.example.indonesiainvestorclub.models.response.AgreementRes;
 import com.example.indonesiainvestorclub.services.CallbackWrapper;
 import com.example.indonesiainvestorclub.services.ServiceGenerator;
@@ -11,6 +12,9 @@ import com.google.gson.JsonElement;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
@@ -50,7 +54,19 @@ public class AgreementViewModel extends BaseViewModelWithCallback {
             AgreementRes agreementRes = new AgreementRes();
 
             jsonObject = new JSONObject(response.toString());
-            JSONObject object = jsonObject.getJSONObject("IIC_MUTUAL_FUND_AGREEMENT");
+            JSONObject objectAgreement = jsonObject.getJSONObject("IIC_MUTUAL_FUND_AGREEMENT");
+
+            List<Agreement> agreement = new ArrayList<>();
+
+            for (int i = 1; i <= objectAgreement.length(); i++) {
+                JSONObject objAgreement = objectAgreement.getJSONObject(i + "");
+
+                String parent = objAgreement.getString("Parent");
+                JSONObject childsObject = objAgreement.getJSONObject("Childs");
+                for (int o = 1; o <= childsObject.length(); o++) {
+                    JSONObject objChilds = childsObject.getJSONObject(o + "");
+                }
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
