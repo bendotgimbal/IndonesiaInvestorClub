95381657c0aee0885dd39abb7c99241804ec6ab2
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2019-12-23 23:01:26 +0700

    Update 23/12/2019 23:00 - Update LoginRes

--- app/src/main/java/com/example/indonesiainvestorclub/models/response/LoginRes.java
+++ app/src/main/java/com/example/indonesiainvestorclub/models/response/LoginRes.java
@@ -6,6 +6,9 @@ public class LoginRes {
   @SerializedName("X-API-KEY")
   private String token;
   private String username;
+  private String avatar;
+  private String firstname;
+  private String lastname;
 
   public String getToken() {
     return token;
@@ -21,5 +24,29 @@ public class LoginRes {
 
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public String getAvatar() {
+    return avatar;
+  }
+
+  public void setAvatar(String avatar) {
+    this.avatar = avatar;
+  }
+
+  public String getFirstName() {
+    return firstname;
+  }
+
+  public void setFirstName(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastName() {
+    return lastname;
+  }
+
+  public void setLastName(String lastname) {
+    this.lastname = lastname;
   }
 }
