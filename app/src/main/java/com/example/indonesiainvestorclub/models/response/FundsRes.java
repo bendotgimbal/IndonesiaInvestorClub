8a267e0b4bd39601bc4de53414f1754e32b40572
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2019-12-30 14:28:29 +0700

    Update 30/12/2019 14:27 - Update FundsViewModel - Update funds_fragment Layout - Update FundsRes - Update string xml

--- app/src/main/java/com/example/indonesiainvestorclub/models/response/FundsRes.java
+++ app/src/main/java/com/example/indonesiainvestorclub/models/response/FundsRes.java
@@ -1,14 +1,17 @@
 package com.example.indonesiainvestorclub.models.response;
 
 import com.example.indonesiainvestorclub.models.Funds;
+import com.example.indonesiainvestorclub.models.Meta;
 
 import java.util.List;
 
 public class FundsRes {
   private List<Funds> funds;
+  private List<Meta> meta;
 
-  public FundsRes(List<Funds> funds) {
+  public FundsRes(List<Funds> funds, List<Meta> meta) {
     this.funds = funds;
+    this.meta = meta;
   }
 
   public List<Funds> getFunds() {
@@ -17,5 +20,13 @@ public class FundsRes {
 
   public void setFunds(List<Funds> funds) {
     this.funds = funds;
+  }
+
+  public List<Meta> getMeta() {
+    return meta;
+  }
+
+  public void setMeta(List<Meta> meta) {
+    this.meta = meta;
   }
 }
