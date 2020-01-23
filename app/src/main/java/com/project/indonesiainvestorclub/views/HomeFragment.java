4d2aba1bfb9c9dbe127a7821909cb843758ec37f
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2020-01-23 14:01:36 +0700

    Commit 23/01/2020 14:01
    - Update HomeFragment
    - Add dialog_negative_round xml
    - Add dialog_positive_round xml
    - Add ic_mood_bad_black_46dp xml
    - Add ic_mood_black_46dp xml
    - Add about_dialog xml
    - Add dialog_negative_layout xml
    - Add dialog_posive_layout xml
    - Update Colors xml
    - Update string xml

--- app/src/main/java/com/project/indonesiainvestorclub/views/HomeFragment.java
+++ app/src/main/java/com/project/indonesiainvestorclub/views/HomeFragment.java
@@ -1,11 +1,16 @@
 package com.project.indonesiainvestorclub.views;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.databinding.DataBindingUtil;
 import com.project.indonesiainvestorclub.R;
 import com.project.indonesiainvestorclub.databinding.HomeFragmentBinding;
@@ -20,16 +25,33 @@ public class HomeFragment extends BaseFragment {
     super.onCreate(savedInstanceState);
   }
 
+  @SuppressLint("NewApi")
   @Nullable @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     super.onCreateView(inflater, container, savedInstanceState);
     binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false);
+    showAboutDialog();
     return binding.getRoot();
   }
 
   @Override protected void initDataBinding() {
     viewModel = new HomeViewModel(this.getContext(), binding);
     binding.setViewModel(viewModel);
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  private void showAboutDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+    builder.setTitle(R.string.about_app);
+    builder.setView(R.layout.about_dialog);
+    builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialogInterface, int i) {
+        // dismiss dialog
+        dialogInterface.dismiss();
+      }
+    });
+    builder.show();
   }
 }
