2e9d252142aff3f95ebb3ac46d98fb1816a4a0f9
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2020-01-31 11:04:43 +0700

    Commit 31/01/2020 11:04
    - Update ProfileEditViewModel
    - Update profile_edit_activity xml
    - Update splash_activity xml

--- app/src/main/java/com/project/indonesiainvestorclub/viewModels/ProfileEditViewModel.java
+++ app/src/main/java/com/project/indonesiainvestorclub/viewModels/ProfileEditViewModel.java
@@ -40,19 +40,6 @@ public class ProfileEditViewModel extends BaseViewModelWithCallback {
     public ObservableField<String> phoneNumberValueTx;
     public ObservableField<String> occupationValueTx;
 
-    private String firstNameValueStr;
-    private String lastNameValueStr;
-    private String dobValueStr;
-    private String maritalStatusValueStr;
-    private String addressValueStr;
-    private String postalCodeValueStr;
-    private String genderValueStr;
-    private String nationalityValueStr;
-    private String cityValueStr;
-    private String countryValueStr;
-    private String phoneNumberValueStr;
-    private String occupationValueStr;
-
     public ProfileEditViewModel(Context context, ProfileEditActivityBinding binding) {
         super(context);
         this.binding = binding;
@@ -93,94 +80,7 @@ public class ProfileEditViewModel extends BaseViewModelWithCallback {
         phoneNumberValueTx.set(phoneNumberStr);
         occupationValueTx.set(occupationStr);
 
-//        firstNameValueStr = firstNameStr;
-//        lastNameValueStr = lastNameStr;
-//        dobValueStr = dobStr;
-//        maritalStatusValueStr = maritalStatusStr;
-//        addressValueStr = addressStr;
-//        postalCodeValueStr = postalCodeStr;
-//        genderValueStr = genderStr;
-//        nationalityValueStr = nationalityStr;
-//        cityValueStr = cityStr;
-//        countryValueStr = countryStr;
-//        phoneNumberValueStr = phoneNumberStr;
-//        occupationValueStr = occupationStr;
-
-//        firstNameValueStr = firstNameValueTx.get();
-//        lastNameValueStr = lastNameValueTx.get();
-//        dobValueStr = dobValueTx.get();
-//        maritalStatusValueStr = maritalStatusValueTx.get();
-//        addressValueStr = addressValueTx.get();
-//        postalCodeValueStr = postalCodeValueTx.get();
-//        genderValueStr = genderValueTx.get();
-//        nationalityValueStr = nationalityValueTx.get();
-//        cityValueStr = cityValueTx.get();
-//        countryValueStr = countryValueTx.get();
-//        phoneNumberValueStr = phoneNumberValueTx.get();
-//        occupationValueStr = occupationValueTx.get();
-//        Toast.makeText(context, "First Name "+firstNameValueStr, Toast.LENGTH_SHORT).show();
-
     }
-
-//    private String getFirstName(){
-//        if (firstNameValueStr == null) return "";
-//        return firstNameValueStr;
-//    }
-//
-//    private String getLastName(){
-//        if (lastNameValueStr == null) return "";
-//        return lastNameValueStr;
-//    }
-//
-//    private String getDOB(){
-//        if (dobValueStr == null) return "";
-//        return dobValueStr;
-//    }
-//
-//    private String getMaritalStatus(){
-//        if (maritalStatusValueStr == null) return "";
-//        return maritalStatusValueStr;
-//    }
-//
-//    private String getAddress(){
-//        if (addressValueStr == null) return "";
-//        return addressValueStr;
-//    }
-//
-//    private String getPostalCode(){
-//        if (postalCodeValueStr == null) return "";
-//        return postalCodeValueStr;
-//    }
-//
-//    private String getGender(){
-//        if (genderValueStr == null) return "";
-//        return genderValueStr;
-//    }
-//
-//    private String getNationality(){
-//        if (nationalityValueStr == null) return "";
-//        return nationalityValueStr;
-//    }
-//
-//    private String getCity(){
-//        if (cityValueStr == null) return "";
-//        return cityValueStr;
-//    }
-//
-//    private String getCountry(){
-//        if (countryValueStr == null) return "";
-//        return countryValueStr;
-//    }
-//
-//    private String getPhoneNumber(){
-//        if (phoneNumberValueStr == null) return "";
-//        return phoneNumberValueStr;
-//    }
-//
-//    private String getOccupation(){
-//        if (occupationValueStr == null) return "";
-//        return occupationValueStr;
-//    }
 
 
     private String getFirstName(){
@@ -189,8 +89,8 @@ public class ProfileEditViewModel extends BaseViewModelWithCallback {
     }
 
     private String getLastName(){
-        if (firstNameValueTx.get() == null) return "";
-        return firstNameValueTx.get();
+        if (lastNameValueTx.get() == null) return "";
+        return lastNameValueTx.get();
     }
 
     private String getDOB(){
@@ -247,26 +147,26 @@ public class ProfileEditViewModel extends BaseViewModelWithCallback {
     public void onClickEditProfile(View view) {
 //        loading(true);
 //        Toast.makeText(context, "Update "+getFirstName(), Toast.LENGTH_SHORT).show();
-        Toast.makeText(context, "Update "+firstNameValueTx.get(), Toast.LENGTH_SHORT).show();
+//        Toast.makeText(context, "Update "+firstNameValueTx.get(), Toast.LENGTH_SHORT).show();
 
-//        Disposable disposable = ServiceGenerator.service.profileUpdateRequest(getFirstName(), getLastName(), getDOB(), getMaritalStatus(), getAddress(), getPostalCode(), getGender()
-//                , getNationality(), getCity(), getCountry(), getPhoneNumber(), getOccupation())
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribeWith(new CallbackWrapper<Response<ProfileUpdateRes>>(this, () -> onClickEditProfile(view)) {
-//                    @Override
-//                    protected void onSuccess(Response<ProfileUpdateRes> updateResponse) {
-//                        String cookie = updateResponse.headers().get("Set-Cookie");
-//                        StringHelper.getCookie(cookie);
-//                        onSuccessUpdate(updateResponse.body());
-//                    }
-//
-//                    @Override public void onNext(Response<ProfileUpdateRes> updateResResponse) {
-//                        super.onNext(updateResResponse);
-////                        loading(false);
-//                    }
-//                });
-//        compositeDisposable.add(disposable);
+        Disposable disposable = ServiceGenerator.service.profileUpdateRequest(getFirstName(), getLastName(), getDOB(), getMaritalStatus(), getAddress(), getPostalCode(), getGender()
+                , getNationality(), getCity(), getCountry(), getPhoneNumber(), getOccupation())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new CallbackWrapper<Response<ProfileUpdateRes>>(this, () -> onClickEditProfile(view)) {
+                    @Override
+                    protected void onSuccess(Response<ProfileUpdateRes> updateResponse) {
+                        String cookie = updateResponse.headers().get("Set-Cookie");
+                        StringHelper.getCookie(cookie);
+                        onSuccessUpdate(updateResponse.body());
+                    }
+
+                    @Override public void onNext(Response<ProfileUpdateRes> updateResResponse) {
+                        super.onNext(updateResResponse);
+//                        loading(false);
+                    }
+                });
+        compositeDisposable.add(disposable);
 
     }
 
