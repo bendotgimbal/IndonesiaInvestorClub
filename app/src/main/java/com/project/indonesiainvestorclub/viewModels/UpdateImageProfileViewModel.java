e1826c0d73a523807bd3a75a070ddf77a41142f5
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2020-02-14 01:39:58 +0700

    Update 14/02/20 01:39
    - Add ImageContract
    - Add ImagePresenter
    - Update UpdateImageProfileViewModel
    - Update UpdateImageProfileActivity
    - Update update_image_profile_activity xml
    - Add FileUtil

--- app/src/main/java/com/project/indonesiainvestorclub/viewModels/UpdateImageProfileViewModel.java
+++ app/src/main/java/com/project/indonesiainvestorclub/viewModels/UpdateImageProfileViewModel.java
@@ -1,25 +1,48 @@
 package com.project.indonesiainvestorclub.viewModels;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.databinding.ObservableBoolean;
 
 import com.project.indonesiainvestorclub.databinding.UpdateImageProfileActivityBinding;
+import com.project.indonesiainvestorclub.helper.ImageHelper;
+import com.project.indonesiainvestorclub.views.UpdateImageProfileActivity;
+
+import java.io.File;
 
 public class UpdateImageProfileViewModel extends BaseViewModelWithCallback {
 
     private UpdateImageProfileActivityBinding binding;
     public ObservableBoolean loadingState;
+    private UpdateImageProfileActivity updateImageProfileActivity;
 
     public UpdateImageProfileViewModel(Context context, UpdateImageProfileActivityBinding binding) {
         super(context);
         this.binding = binding;
+        this.updateImageProfileActivity = updateImageProfileActivity;
         loadingState = new ObservableBoolean(false);
 
     }
 
+//    public void start(String image) {
+//        ImageHelper.loadImage(binding.ivPickImage, image);
+//    }
+
+    public void start(File fileimage) {
+        if (fileimage == null) return;
+        ImageHelper.loadImage(binding.ivPickImage, fileimage.getAbsolutePath());
+        Toast.makeText(context, "Image Name " +fileimage.getName(), Toast.LENGTH_LONG).show();
+    }
+
     private void loading(boolean load){
         loadingState.set(load);
+    }
+
+    @SuppressWarnings("unused")
+    public void onButtonUpdateImageClick(View view) {
+        ((UpdateImageProfileActivity)getContext()).chooseGallery();
     }
 
     @Override public void hideLoading() {
