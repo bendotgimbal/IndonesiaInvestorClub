1578509678c9fbf322a070ff42f7048ca626b602
robertw <robert.w.ruhulessin@alfacart.com>
2020-02-14 11:20:39 +0700

    + show image preview

--- app/src/main/java/com/project/indonesiainvestorclub/viewModels/UpdateImageProfileViewModel.java
+++ app/src/main/java/com/project/indonesiainvestorclub/viewModels/UpdateImageProfileViewModel.java
@@ -1,9 +1,12 @@
 package com.project.indonesiainvestorclub.viewModels;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.databinding.ObservableBoolean;
 
 import com.project.indonesiainvestorclub.databinding.UpdateImageProfileActivityBinding;
@@ -14,38 +17,31 @@ import java.io.File;
 
 public class UpdateImageProfileViewModel extends BaseViewModelWithCallback {
 
-    private UpdateImageProfileActivityBinding binding;
-    public ObservableBoolean loadingState;
-    private UpdateImageProfileActivity updateImageProfileActivity;
+  private UpdateImageProfileActivityBinding binding;
+  public ObservableBoolean loadingState;
 
-    public UpdateImageProfileViewModel(Context context, UpdateImageProfileActivityBinding binding) {
-        super(context);
-        this.binding = binding;
-        this.updateImageProfileActivity = updateImageProfileActivity;
-        loadingState = new ObservableBoolean(false);
+  public UpdateImageProfileViewModel(Context context, UpdateImageProfileActivityBinding binding) {
+    super(context);
+    this.binding = binding;
+    loadingState = new ObservableBoolean(false);
+  }
 
-    }
+  public void start(File fileimage) {
+    if (fileimage == null) return;
+    ImageHelper.loadLocalImage(binding.ivPickImage, fileimage.getAbsolutePath());
+    Toast.makeText(context, "Image Name " + fileimage.getName(), Toast.LENGTH_LONG).show();
+  }
 
-//    public void start(String image) {
-//        ImageHelper.loadImage(binding.ivPickImage, image);
-//    }
+  private void loading(boolean load) {
+    loadingState.set(load);
+  }
 
-    public void start(File fileimage) {
-        if (fileimage == null) return;
-        ImageHelper.loadImage(binding.ivPickImage, fileimage.getAbsolutePath());
-        Toast.makeText(context, "Image Name " +fileimage.getName(), Toast.LENGTH_LONG).show();
-    }
+  @SuppressWarnings("unused")
+  public void onButtonUpdateImageClick(View view) {
+    ((UpdateImageProfileActivity) getContext()).chooseGallery();
+  }
 
-    private void loading(boolean load){
-        loadingState.set(load);
-    }
-
-    @SuppressWarnings("unused")
-    public void onButtonUpdateImageClick(View view) {
-        ((UpdateImageProfileActivity)getContext()).chooseGallery();
-    }
-
-    @Override public void hideLoading() {
-        loading(false);
-    }
+  @Override public void hideLoading() {
+    loading(false);
+  }
 }
