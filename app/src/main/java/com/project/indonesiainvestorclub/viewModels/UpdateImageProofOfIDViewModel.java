a158a7898e77b5825c9f26f0376865cb6b7cfb23
bendotgimbal <yosua.tony.batara.hutagalung@gmail.com>
2020-02-14 17:33:45 +0700

    Update 14/02/2020 17:33
    - Update UpdateImageProofOfIDViewModel
    - Update UpdateImageProofOfIDActivity
    - Update update_image_proof_of_id xml

--- app/src/main/java/com/project/indonesiainvestorclub/viewModels/UpdateImageProofOfIDViewModel.java
+++ app/src/main/java/com/project/indonesiainvestorclub/viewModels/UpdateImageProofOfIDViewModel.java
@@ -1,10 +1,16 @@
 package com.project.indonesiainvestorclub.viewModels;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.databinding.ObservableBoolean;
 
 import com.project.indonesiainvestorclub.databinding.UpdateImageProofOfIdActivityBinding;
+import com.project.indonesiainvestorclub.helper.ImageHelper;
+import com.project.indonesiainvestorclub.views.UpdateImageProofOfIDActivity;
+
+import java.io.File;
 
 public class UpdateImageProofOfIDViewModel extends BaseViewModelWithCallback {
 
@@ -18,8 +24,19 @@ public class UpdateImageProofOfIDViewModel extends BaseViewModelWithCallback {
 
     }
 
+    public void start(File fileimage) {
+        if (fileimage == null) return;
+        ImageHelper.loadLocalImage(binding.ivPickImage, fileimage.getAbsolutePath());
+        Toast.makeText(context, "Image Name " + fileimage.getName(), Toast.LENGTH_LONG).show();
+    }
+
     private void loading(boolean load){
         loadingState.set(load);
+    }
+
+    @SuppressWarnings("unused")
+    public void onButtonUpdateImageClick(View view) {
+        ((UpdateImageProofOfIDActivity) getContext()).chooseGallery();
     }
 
     @Override public void hideLoading() {
--- app/src/main/java/com/project/indonesiainvestorclub/views/UpdateImageProofOfIDActivity.java
+++ app/src/main/java/com/project/indonesiainvestorclub/views/UpdateImageProofOfIDActivity.java
@@ -1,17 +1,27 @@
 package com.project.indonesiainvestorclub.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import androidx.databinding.DataBindingUtil;
 
 import com.project.indonesiainvestorclub.R;
 import com.project.indonesiainvestorclub.databinding.UpdateImageProofOfIdActivityBinding;
+import com.project.indonesiainvestorclub.helper.FileUtil;
 import com.project.indonesiainvestorclub.viewModels.UpdateImageProofOfIDViewModel;
+
+import java.io.File;
+import java.io.IOException;
+
+import io.reactivex.annotations.Nullable;
 
 public class UpdateImageProofOfIDActivity extends BaseActivity {
 
     private UpdateImageProofOfIdActivityBinding binding;
     private UpdateImageProofOfIDViewModel viewModel;
+    static final int REQUEST_GALLERY_PHOTO = 102;
+    File mPhotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,28 @@ public class UpdateImageProofOfIDActivity extends BaseActivity {
         binding = DataBindingUtil.setContentView(this, R.layout.update_image_proof_of_id_activity);
         viewModel = new UpdateImageProofOfIDViewModel(this, binding);
         binding.setViewModel(viewModel);
+    }
+
+    public void chooseGallery() {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickPhoto.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivityForResult(pickPhoto, REQUEST_GALLERY_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_GALLERY_PHOTO) {
+                try {
+                    mPhotoFile = FileUtil.from(this, data.getData());
+                    viewModel.start(mPhotoFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
